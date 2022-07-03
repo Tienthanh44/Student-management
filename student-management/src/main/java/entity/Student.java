fad74b491e16a6e4b952a2ab1b1374c28b659fdc
Tienthanh44 <thanhpanda954@gmail.com>
2022-07-03 21:43:13 +0700

    first commit

---
+++ student-management/src/main/java/entity/Student.java
@@ -0,0 +1,63 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "Id")
+    private Integer id;
+
+    @Column(name = "StudentName")
+    private String studentName;
+
+    @Column(name = "Age")
+    private Integer age;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "CreatedDate")
+    private java.sql.Date createdDate;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public java.sql.Date getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public void setCreatedDate(java.sql.Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
